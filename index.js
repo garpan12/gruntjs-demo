f17b64e50e413a4037d8f290e9edcab1620fcf37
Garwood Pang <garwood.pang@alumni.ubc.ca>
2013-11-07 14:11:32 -0800

    updated index page

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function ()
+{
+    $('#example').popover({
+        placement : 'bottom',
+        title : 'test',
+        content : 'gewagwag'
+    });
+    $('#example2').popover({
+        placement : 'bottom',
+        title : 'test',
+        content : 'gewagwag'
+    });
+    
+});
+
