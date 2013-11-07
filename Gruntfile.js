787d0b7b39938d10ae0e546722c73c551bfb8817
Julien Law <julienlaw1@gmail.com>
2013-11-07 01:19:14 -0800

    added qunit, jshint and mocha tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
     // initialize the configuration of the Grunt workflow
     grunt.initConfig({
     
+        // this is the package file that we are going to use
         pkg: grunt.file.readJSON('package.json'),
         
         // creating the uglify task
@@ -20,17 +21,42 @@ module.exports = function(grunt) {
         },
         
         jasmine: {
+            
+        },
         
+        qunit: {
+            // location of the test runner files
+            files: ['test/**/*.html']
+        },
+        
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        
+        mocha: {
+            all: ['test/**/*.html']
         }
     });
     
     // load the plugin
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha');
     
     // set the tasks alias
     
     // let default grunt build do all the things in the config
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['qunit', 'uglify']);
     // uglify task
     grunt.registerTask('uglify', 'uglify', function() {
         grunt.log.write('Uglifying the project...').ok();
@@ -39,4 +65,14 @@ module.exports = function(grunt) {
     grunt.registerTask('jasmine', 'jasmine', function() {
         grunt.log.write('Checking the Jasmine tests...').ok();
     });
+    // QUnit task
+    grunt.registerTask('qunit', 'qunit', function() {
+        grunt.log.write('Checking the QUnit tests...').ok();
+    });
+    // JSHint task
+    grunt.registerTask('jshint', 'jshint', function() {
+        grunt.log.write('Validating against JSHint...').ok();
+    });
+    // Mocha task
+    grunt.registerTask('mocha', 'mocha');
 };
