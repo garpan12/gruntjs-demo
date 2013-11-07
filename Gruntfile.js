122227107b775ac584bb85753da2218512c3b0f4
Julien Law <julienlaw1@gmail.com>
2013-11-07 00:45:35 -0800

    added uglify to the Gruntfile and updated the package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,42 @@
+// wrapper function for Grunt
 module.exports = function(grunt) {
-    // set up grunt
-    grunt.initConfig({
-        jshint: {
-            src:['Gruntfile.js', 'src/config.js'],
-            option: {
-                curly: true,
-                
 
+    // initialize the configuration of the Grunt workflow
+    grunt.initConfig({
+    
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // creating the uglify task
+        uglify: {
+            // override the default options
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyy-mm-dd") %> */\n'
+            },
+            
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        
+        jasmine: {
+        
         }
     });
-
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-
-    grunt.registerTask('default', 'lint');
+    
+    // load the plugin
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    // set the tasks alias
+    
+    // let default grunt build do all the things in the config
+    grunt.registerTask('default', ['uglify']);
+    // uglify task
+    grunt.registerTask('uglify', 'uglify', function() {
+        grunt.log.write('Uglifying the project...').ok();
+    });
+    // jasmine task
+    grunt.registerTask('jasmine', 'jasmine', function() {
+        grunt.log.write('Checking the Jasmine tests...').ok();
+    });
 };
