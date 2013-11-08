2b1ef66c10b75aeaf33b060367bb21c5079dee05
Julien Law <julienlaw1@gmail.com>
2013-11-07 22:27:32 -0800

    added IDs to the questions

--- src/js/index.js
+++ src/js/index.js
@@ -3,27 +3,32 @@ $(function ()
     $('#example').popover({
         placement : 'bottom',
         title : 'Question 1',
-        content : 'What Programming Language is Grunt in?'
+        html: true,
+        content: '<div id="q1-popover">What Programming Language is Grunt in?</div>'
     });
     $('#example2').popover({
         placement : 'bottom',
         title : 'Question 2',
-        content : 'Name a Grunt Plugin and Explain What it Does.'
+        html: true,
+        content: '<div id="q2-popover">Name a Grunt Plugin and Explain What it Does.</div>'
     });
     $('#example3').popover({
         placement : 'bottom',
         title : 'Question 3',
-        content : 'What does grunt.loadNpmTasks() do?'
+        html: true,
+        content: '<div id="q3-popover">What does grunt.loadNpmTasks() do?</div>',
     });
    $('#example4').popover({
         placement : 'bottom',
         title : 'Question 4',
-        content : ''
+        html: true,
+        content: '<div id="q4-popover">Explain how you would create a task in the Gruntfile</div>'
     });
    $('#example5').popover({
         placement : 'bottom',
         title : 'Question 5',
-        content : 'What is Our Group Number?'
+        html: true,
+        content: '<div id="q5-popover">What is Our Group Number?</div>'
     });
    
 });
