aecbe831892813e2cacd227a7f86488656c4c570
Julien Law <julienlaw1@gmail.com>
2013-11-07 22:27:14 -0800

    fixed the unit test to work with popover

--- test/demo.js
+++ test/demo.js
@@ -1,5 +1,7 @@
 test('hello', function() {
     expect(1);
-    var button = document.getElementById("example");
-    equal(button.innerText, "hello", "DOM accessed");
+    var button = jQuery("#example");
+    button.click();
+    var popover = jQuery("div#q1-popover");
+    equal(popover.html(), "What Programming Language is Grunt in?", "DOM accessed");
 });
